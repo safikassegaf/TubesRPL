5a9badbe72ba9987f9824e091725c682a2447f09
safikassegaf <msafikasegaf@gmail.com>
2022-04-03 22:31:02 +0700

    update: signup form ref #4

--- TubesRPL/resources/views/signup/form.blade.php
+++ TubesRPL/resources/views/signup/form.blade.php
@@ -1,31 +1,28 @@
 @extends('Layout.loginLayout')
 
 @section('login')
-    <form action="/login" method="POST">
-        @csrf
-        <h4>{{ $namaRole }} signup</h4>
-        <a href="/login/student" class="">Sign in as Student </a>
-        <div class="mb-3 mt-4 row">
-            <div class="col">
-                <input type="text" class="form-control" id="email" name="email" placeholder="Email Adress"
-                    value="{{ old('email') }}">
-            </div>
+<form action="/login" method="POST">
+    @csrf
+    <h4>{{ $namaRole }} signup</h4>
+    <div class="mb-3 mt-4 row">
+        <div class="col">
+            <input type="text" class="form-control" id="email" name="email" placeholder="Email Adress" value="{{ old('email') }}">
         </div>
-        <div class=" mb-4 row">
-            <div class="col">
-                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-            </div>
+    </div>
+    <div class=" mb-4 row">
+        <div class="col">
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
         </div>
-        <div class="form-check mb-4 ">
-            <input class="form-check-input" type="checkbox" value="" id="remember" name="remember" checked>
-            <label class="form-check-label" for="flexCheckChecked">
-                Remember me
-            </label>
+    </div>
+    <div class=" mb-4 row ">
+        <div class="col">
+            <input class="form-control" type="password" id="passwordconf" name="passwordconf" placeholder=" Re-enter Password">
         </div>
-        <div class="d-grid gap-2 col-12 mx-auto">
-            <button type="submit" class="btn ">Get Started</button>
-        </div>
-        <input type="text" hidden name="role_id" id="role_id" value="1">
-        <p class="text-center">Don't Have an account? <a href="/signup">Create For Free</a></p>
-    </form>
+    </div>
+    <div class="d-grid gap-2 col-12 mx-auto">
+        <button type="submit" class="btn ">Get Started</button>
+    </div>
+    <input type="text" hidden name="role_id" id="role_id" value="{{$role_id}}">
+    <p class="text-center">Already have an account? <a href="/login/student">Sign in</a></p>
+</form>
 @endsection
